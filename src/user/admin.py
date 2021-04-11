b7384d994fa1fc0656f55b8b781aefa9cb320ac3
haseefathi <hasee.fathi@gmail.com>
2021-04-11 23:13:09 +0400

    added fields to siteuser

--- src/user/admin.py
+++ src/user/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+from .models import SiteUser
+
+
+class SiteUserInline(admin.StackedInline):
+    model = SiteUser
+    can_delete = False
+    verbose_name_plural = 'siteuser'
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (SiteUserInline,)
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
--- src/user/models.py
+++ src/user/models.py
@@ -1,7 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+score_size = models.IntegerField(validators = [MinValueValidator(130), MaxValueValidator(170)] )
 
 # Create your models here.
 class SiteUser(models.Model):
     user = models.OneToOneField(User, on_delete = models.CASCADE)
-    first_name = models.CharField(max_length=50)
+    # degree = models.CharField()
+    gre_verbal_score = models.IntegerField(default= 150, validators = [MinValueValidator(130), MaxValueValidator(170)], blank = True)
+    gre_quant_score = models.IntegerField(default= 152, validators = [MinValueValidator(130), MaxValueValidator(170)], blank = True)
+    gre_awa_score = models.DecimalField(default=3.5, max_digits = 2, decimal_places = 1,  validators = [MinValueValidator(0.0), MaxValueValidator(6.0)], blank = True)
+    intended_semester = models.CharField(max_length = 5, default = 'F', blank = True)
+    toefl_score = models.IntegerField(default= 152, validators = [MinValueValidator(130), MaxValueValidator(170)], blank = True)
+    undergrad_gpa = models.DecimalField(default=3.0, max_digits = 2, decimal_places = 1,  validators = [MinValueValidator(0.0), MaxValueValidator(6.0)], blank = True)
