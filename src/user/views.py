1e599af82926fd22960d0c792ac28730fd653485
haseefathi <hasee.fathi@gmail.com>
2021-04-12 21:21:38 +0400

    removed unnecessary imports

--- src/user/views.py
+++ src/user/views.py
@@ -1,25 +1,11 @@
-from django.contrib.auth.forms import UserCreationForm
-from django.urls import reverse_lazy
-from django.views import generic
-
 from django.shortcuts import redirect, render
 
 from .forms import SignUpForm
 
 from django.contrib.auth.models import User
 from user.models import SiteUser
+
 from django.contrib.auth import authenticate, login
-
-# def user_signup_view(request):
-#     form = SignUpForm(request.POST or None)
-#     if form.is_valid():
-#         form.save()
-#         form = SignUpForm()
-#     context = {
-#         'form': form
-#     }
-#     return render(request, 'registration/signup.html', context)
-
 
 def user_signup_view(request):
     form = SignUpForm(request.POST or None)
@@ -54,12 +40,4 @@ def user_signup_view(request):
         'form': form
     }
     return render(request, 'registration/signup.html', context)
-
-
-
-class SignUpView(generic.CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'registration/signup.html'
-
 
