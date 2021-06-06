13cc6c75a52e1e3d0d6187daf0402ca49efb15fa
haseefathi <hasee.fathi@gmail.com>
2021-06-06 22:57:43 +0400

    styled chat page

--- src/pages/static/js/chat_script.js
+++ src/pages/static/js/chat_script.js
@@ -20,3 +20,30 @@ function open_chat(username) {
     xhttp.open("GET", chat_url, true);
     xhttp.send();
 }
+
+
+function search_messages() {
+    var input, container_classname, value_classname, filter, i;
+    // console.log('searching');
+    input = document.getElementById('search_messages_input');
+    value_classname = 'name_container';
+
+    filter = input.value.toUpperCase().trim();
+    let all_messages = document.getElementsByClassName("user_box");
+
+    for (i = 0; i < all_messages.length; i++) {
+
+        let value = all_messages[i].getElementsByClassName(value_classname)[0];
+        value = value.innerText.toUpperCase().trim();
+        console.log('value ' + value);
+
+        let user_cell_element = all_messages[i];
+
+        if (value.indexOf(filter) > -1) {
+            user_cell_element.style.display = "block";
+        } else {
+            user_cell_element.style.display = "none";
+        }
+
+    }
+}
