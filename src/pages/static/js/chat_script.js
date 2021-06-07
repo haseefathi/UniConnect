4547152d0631c208cff1159678bbeb3eb7482749
haseefathi <hasee.fathi@gmail.com>
2021-06-07 23:15:10 +0400

    did add messages modal

--- src/pages/static/js/chat_script.js
+++ src/pages/static/js/chat_script.js
@@ -47,3 +47,45 @@ function search_messages() {
 
     }
 }
+
+
+function search_friends() {
+    var input, container_classname, value_classname, filter, i;
+    // console.log('searching');
+    input = document.getElementById('search_friends_input');
+    value_classname = 'name_container';
+
+    filter = input.value.toUpperCase().trim();
+    let all_messages = document.getElementsByClassName("new_message_user_box");
+
+    for (i = 0; i < all_messages.length; i++) {
+
+        let value = all_messages[i].getElementsByClassName(value_classname)[0];
+        value = value.innerText.toUpperCase().trim();
+        console.log('value ' + value);
+
+        let user_cell_element = all_messages[i];
+
+        if (value.indexOf(filter) > -1) {
+            user_cell_element.style.display = "block";
+        } else {
+            user_cell_element.style.display = "none";
+        }
+
+    }
+}
+
+function addMessagesModal() {
+    // Get the modal
+    var modal = document.getElementById("add_messages_modal");
+    // // When the user clicks the button, open the modal 
+    modal.style.display = "block";
+    // Get the cancel button
+    var close = document.getElementById("close_button");
+
+    // When the user clicks on <span> (x), close the modal
+    close.onclick = function() {
+        modal.style.display = "none";
+    }
+
+}
--- src/pages/static/js/main-script.js
+++ src/pages/static/js/main-script.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-    showFriends();
+    showHome();
 };
 
 function scrollToTop() {
